1a3d54e0d85334332ce674073dac825d4d574ed5
Luciano Muniz <luciano.mo@gmail.com>
2022-11-08 00:05:19 -0300

    ...

--- components/loading/loading.js
+++ components/loading/loading.js
@@ -1,7 +1,7 @@
 import styles from './loading.module.css';
 
 const Loading = () => {
-  return <p className={styles.loader}>Loading...</p>;
+  return <p className={styles.loader}>Loading....</p>;
 };
 
 export default Loading;
